88d0f8991a5168c6abcff950c1dca03a246f4d51
Tobias Kupek <mail@tkupek.eu>
2020-03-17 13:18:32 +0100

    update calibration to use final video

--- src/camera_calibration.py
+++ src/camera_calibration.py
@@ -1,7 +1,8 @@
 import numpy as np
 import cv2
+import os
 
-VIDEO_PATH = './video/test.mp4'
+VIDEO_PATH = '../data/visual-geometry-calibration.MTS'
 
 # checkerboard characteristics
 SQUARE_SIZE = 25
@@ -11,8 +12,8 @@ BOARD_SIZE = (8, 6)
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, SQUARE_SIZE, 0.001)
 
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((6 * 7, 3), np.float32)
-objp[:, :2] = np.mgrid[0:7, 0:6].T.reshape(-1, 2)
+objp = np.zeros((BOARD_SIZE[0] * BOARD_SIZE[1], 3), np.float32)
+objp[:, :2] = np.mgrid[0:BOARD_SIZE[0], 0:BOARD_SIZE[1]].T.reshape(-1, 2)
 
 # Arrays to store object points and image points from all the images.
 objpoints = []  # 3d point in real world space
@@ -25,15 +26,18 @@ print('read', number_frames, 'frames ...')
 success = 1
 count = 0
 test_img = None
-while success: 
+while success:
 
     # function extract frames 
-    success, img = video_cap.read()     
+    success, img = video_cap.read()
     count += 1
-    if count == number_frames // 2: # get test image from middle of video
+
+    # get test image from middle of video
+    if count == number_frames // 2:
         test_img = img
 
-    if not success or count % 10 != 0: # use every 10th element for calibration
+    # use every 10th element for calibration
+    if not success or count % 7 != 0:
         continue
 
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -41,17 +45,18 @@ while success:
     ret, corners = cv2.findChessboardCorners(gray, BOARD_SIZE, None)
 
     # If found, add object points, image points (after refining them)
-    if ret == True:
+    if ret:
         objpoints.append(objp)
 
         corners2 = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
         imgpoints.append(corners2)
 
         # Draw and display the corners
-        #img = cv2.drawChessboardCorners(img, BOARD_SIZE, corners2, ret)
-        #cv2.imshow('img', img)
-        #cv2.waitKey(50)
+        # img = cv2.drawChessboardCorners(img, BOARD_SIZE, corners2, ret)
+        # cv2.imshow('img', img)
+        # cv2.waitKey(50)
 
+print('reading in complete, calculating matrix...')
 cv2.destroyAllWindows()
 
 gray = cv2.cvtColor(test_img, cv2.COLOR_BGR2GRAY)
@@ -62,16 +67,11 @@ h, w = test_img.shape[:2]
 newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w, h), 1, (w, h))
 
 # undistort
-dst = cv2.undistort(test_img, mtx, dist, None, newcameramtx)
-
-# crop the image
-x, y, w, h = roi
-dst = dst[y:y + h, x:x + w]
-
-# show orignal vs. undistorted image
-cv2.imshow("Orignal", test_img)
-cv2.imshow("Undistorted", dst)
-cv2.waitKey(0)
+# dst = cv2.undistort(test_img, mtx, dist, None, newcameramtx)
+# x, y, w, h = roi
+# dst = dst[y:y + h, x:x + w]
+# cv2.imshow("Orignal", test_img)
+# cv2.imshow("Undistorted", dst)
 
 mean_error = 0
 for i in range(len(objpoints)):
@@ -80,3 +80,6 @@ for i in range(len(objpoints)):
     mean_error += error
 
 print("total error: ", mean_error / len(objpoints))
+print(newcameramtx)
+
+np.save(os.path.join('..', 'tmp', 'cmatrix.npy'), newcameramtx)

f4bd971d1cf3dc64013293cc8ed99265ab7cfa6b
moejoe95 <j.gugglberger@gmail.com>
2020-03-19 09:59:27 +0100

    remove incorrect correspondances

--- src/point_correspondance.py
+++ src/point_correspondance.py
@@ -1,4 +1,4 @@
-from __future__ import print_function
+#!/usr/bin/python3
 import cv2
 import numpy as np
 
@@ -38,24 +38,31 @@ def getHomograhpy(im1, im2, save_image=True):
   # calculate mean and standard deviation
   mean = np.mean(points1, axis=0)
   sd = np.std(points1, axis=0)
+  x_lower, x_upper = int(mean[0] - sd[0]), int(mean[0] + sd[0])
+  y_lower, y_upper = int(mean[1] - sd[1]), int(mean[1] + sd[1])
+
+  points1_exact = []
+  points2_exact = []
+  matchesMask = [0 for i in range(len(matches))]
+
+  # remove incorrect matches
+  for i, match in enumerate(matches):
+    (x, y) = keypoints1[match.queryIdx].pt
+    if x_lower < x < x_upper and y_lower < y < y_upper:
+      matchesMask[i]=1
+      points1_exact.append(keypoints1[match.queryIdx].pt)
+      points2_exact.append(keypoints2[match.trainIdx].pt)
+
+  print('got', len(points1_exact), 'matches')
 
   if save_image:
-        # Need to draw only good matches, so create a mask
-    matchesMask = [0 for i in range(len(points1))]
-
-    # ratio test as per Lowe's paper
-    for i, (x, y) in enumerate(points1):
-        if x > mean[0]-sd[0] and x < mean[0]+sd[0] and y > mean[1]-sd[1] and y < mean[1]+sd[1]:
-            matchesMask[i]=1
-
     imMatches = cv2.drawMatches(im1, keypoints1, im2, keypoints2, matches, None, matchColor=(0,255,0), matchesMask=matchesMask)
-    pt1 = (int(mean[0]-sd[0]), int(mean[1]-sd[1]))
-    pt2 = (int(mean[0]+sd[0]), int(mean[1]+sd[1]))
-    imMatches = cv2.rectangle(imMatches, pt1, pt2, (0, 0, 255), 3)
-    cv2.imwrite("../data/matches.jpg", imMatches)
+    # draw red rectangle of standard deviations
+    imMatches = cv2.rectangle(imMatches, (x_lower, y_lower), (x_upper, y_upper), (0, 0, 255), 3)
+    cv2.imwrite("../tmp/matches.jpg", imMatches)
   
   # Find homography
-  H, _ = cv2.findHomography(points1, points2, cv2.RANSAC)
+  H, _ = cv2.findHomography(np.array(points1_exact), np.array(points2_exact), cv2.RANSAC)
 
   return H
 

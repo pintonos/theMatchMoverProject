4ec006c5032de02bf5310369123d11c3bac669b8
moejoe95 <j.gugglberger@gmail.com>
2020-03-30 20:05:27 +0200

    map images between frames

--- src/stereo_manuel.py
+++ src/stereo_manuel.py
@@ -3,6 +3,13 @@ import numpy as np
 from matplotlib import pyplot as plt
 from Constants import *
 import pandas as pd
+
+
+def draw(img, corners, imgpts):
+    imgpts = np.int32(imgpts).reshape(-1, 2)
+    cv2.line(img, tuple(corners[0]), tuple(imgpts[0].ravel()), (255,0,0), 5)
+    cv2.line(img, tuple(corners[1]), tuple(imgpts[1].ravel()), (0,255,0), 5)
+    cv2.line(img, tuple(corners[2]), tuple(imgpts[2].ravel()), (0,0,255), 5)
  
 
 # read manuel points
@@ -32,8 +39,31 @@ print("R:\n", R)
 print("t:\n", t)
 
 
-# TODO insert wireframe in img1 and map them to img2
+# define 3D shape
+axis = np.float32([[3,0,0], [0,3,0], [0,0,-3]]).reshape(-1,3)
+corners = np.intc([[858, 580], [858, 580], [858, 580]])
 
+# project points to 2D image
+imgpts1, jac = cv2.projectPoints(axis, R, t, K, dist)
+
+img1 = cv2.imread('../data/img1.jpg')
+
+# draw lines in image
+draw(img1, corners, imgpts1)
+cv2.imshow('img1', img1)
+cv2.waitKey(0)
 
 # points can be mapped from the first image to the second by the formula: p2 = R*p1 + t
 # see opencv link above
+imagepts2 = []
+for imagept in imgpts1:
+    imagept = np.append(imagept, 1)
+    imgpt2 = np.add(np.matmul(R, imagept), t) # map from img1 to img2
+    imagepts2.append(imgpt2)
+
+img2 = cv2.imread('../data/img2.jpg')
+
+# draw lines in image
+draw(img2, corners, imgpts1)
+cv2.imshow('img2', img2)
+cv2.waitKey(0)

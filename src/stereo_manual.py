535383725eacd97cac9bd9387625476e2ef38976
moejoe95 <j.gugglberger@gmail.com>
2020-04-01 19:50:34 +0200

    update manual stereo

--- src/stereo_manual.py
+++ src/stereo_manual.py
@@ -4,40 +4,47 @@ from matplotlib import pyplot as plt
 from Constants import *
 import pandas as pd
 
-np.set_printoptions(suppress=True)
 
-# https://stackoverflow.com/questions/30207467/how-to-draw-3d-coordinate-axes-with-opencv-for-face-pose-estimation
-def draw_axis(img, R, t, K):
-    # unit is mm
-    rotV, _ = cv2.Rodrigues(R)
-    points = np.float32([[100, 0, 0], [0, 100, 0], [0, 0, 100], [0, 0, 0]]).reshape(-1, 3)
-    axisPoints, _ = cv2.projectPoints(points, rotV, t, K, (0, 0, 0, 0))
-    img = cv2.line(img, tuple(axisPoints[3].ravel()), tuple(axisPoints[0].ravel()), (255,0,0), 3)
-    img = cv2.line(img, tuple(axisPoints[3].ravel()), tuple(axisPoints[1].ravel()), (0,255,0), 3)
-    img = cv2.line(img, tuple(axisPoints[3].ravel()), tuple(axisPoints[2].ravel()), (0,0,255), 3)
-    return img
+def draw_lines(im, center, obj_pts):
+    for pt in obj_pts:
+        cv2.line(im, center, pt, (255, 0, 0), 5)
 
+def draw_points(img, pts):
+    for pt in pts:
+        cv2.circle(img, (pt[0],pt[1]), 3, (0, 0, 255), -1)
 
-def draw(img, corners, imgpts):
-    imgpts = np.int32(imgpts).reshape(-1, 2)
-    cv2.line(img, tuple(corners[0]), tuple(imgpts[0].ravel()), (255, 0, 0), 5)
-    cv2.line(img, tuple(corners[1]), tuple(imgpts[1].ravel()), (0, 255, 0), 5)
-    cv2.line(img, tuple(corners[2]), tuple(imgpts[2].ravel()), (0, 0, 255), 5)
+# transforms point according to formula p2 = R*p1 + t returns a tuple (x, y)
+def transform_point(pt1, R, t):
+    # from image to homogeneuos
+    pt1_homo = np.append(pt1, 1).reshape(3,1)
+    # map from img1 to img2
+    pt_rot = np.matmul(R, pt1_homo)
+    pt2_hom = np.add(pt_rot, t) 
+    # back from homogeneous to image
+    pt2 = pt2_hom / pt2_hom[2]
+    # reshape to int tuple
+    return (int(pt2[0][0]), int(pt2[1][0])) 
 
-
-# define 3D shape
-axis = np.float32([[3,0,0], [0,3,0], [0,0,-3]]).reshape(-1,3)
-corners = np.intc([[858, 580], [858, 580], [858, 580]])
 
 # read manuel points
 pts1 = pd.read_csv('../tmp/manual_pt1.csv', sep=',').values
 pts2 = pd.read_csv('../tmp/manual_pt2.csv', sep=',').values
 
-# get Fundamental matrix
-F, mask = cv2.findFundamentalMat(pts1, pts2, cv2.FM_LMEDS)
-print("F:\n", F)
+img1 = cv2.imread('../data/img1.jpg')
 
-# Load previously saved data
+center = (464, 302)
+obj_pts1 = [(464,252), (514,302),(428,338)]
+
+img1 = cv2.resize(img1, (960, 540))
+draw_lines(img1, center, obj_pts1)
+draw_points(img1, pts1)
+
+cv2.imshow('img1', img1)
+cv2.waitKey(0)
+
+# map to second image
+
+# Load K and dist from calibration
 K, dist = np.load(MAT_CAMERA), np.load(MAT_DIST_COEFF)
 
 # https://stackoverflow.com/questions/33906111/how-do-i-estimate-positions-of-two-cameras-in-opencv
@@ -45,11 +52,10 @@ K, dist = np.load(MAT_CAMERA), np.load(MAT_DIST_COEFF)
 pts_l_norm = cv2.undistortPoints(np.expand_dims(pts1, axis=1).astype(dtype=np.float32), cameraMatrix=K, distCoeffs=dist)
 pts_r_norm = cv2.undistortPoints(np.expand_dims(pts2, axis=1).astype(dtype=np.float32), cameraMatrix=K, distCoeffs=dist)
 
-E, _ = cv2.findEssentialMat(pts_l_norm, pts_r_norm, method=cv2.RANSAC, prob=0.999, threshold=3.0) # TODO test different settings
-print("E:\n", E)
+E, _ = cv2.findEssentialMat(pts_l_norm, pts_r_norm, method=cv2.RANSAC, prob=0.999, threshold=0.1) # TODO test different settings
 
 # recover relative camera rotation and translation from essential matrix and the corresponding points
-points, R, t, mask = cv2.recoverPose(E, pts1, pts2)
+_, R, t, _ = cv2.recoverPose(E, pts1, pts2)
 
 # IMPORTANT: R and t may need to be inverted:
 # see https://answers.opencv.org/question/31421/opencv-3-essentialmatrix-and-recoverpose/
@@ -57,33 +63,21 @@ points, R, t, mask = cv2.recoverPose(E, pts1, pts2)
 #R = np.linalg.inv(R)
 #t = np.flip(t)
 
-print("R:\n", R)
-print("t:\n", t)
+# transform points
+obj_pts2 = []
+for imagept in obj_pts1:
+    obj_pts2.append(transform_point(imagept, R, t))
+    
+# transform center
+center = transform_point(center, R, t)
 
-img1 = cv2.imread('../data/img1.jpg')
-
-# project points to 2D image
-imgpts1, jac = cv2.projectPoints(axis, R, t, K, dist)
-
-# draw lines in image
-draw(img1, corners, imgpts1)
-#draw_axis(img1, R, t, K)
-cv2.imshow('img1', img1)
-cv2.waitKey(0)
-
-
-# points can be mapped from the first image to the second by the formula: p2 = R*p1 + t
-# see opencv link above
-imagepts2 = []
-for imagept in imgpts1:
-    imagept = np.append(imagept, 1)
-    imgpt2 = np.add(np.matmul(R, imagept), t) # map from img1 to img2
-    imagepts2.append(imgpt2)
+print(obj_pts2)
+print(center)
 
 img2 = cv2.imread('../data/img2.jpg')
-
-# draw lines in image
-draw(img2, corners, imgpts1)
+img2 = cv2.resize(img2, (960, 540))
+draw_lines(img2, center, obj_pts2)
+draw_points(img2, pts2)
 
 cv2.imshow('img2', img2)
 cv2.waitKey(0)

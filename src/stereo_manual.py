6d64250b60f1c0c1b84f2534925a3e5a54e00d15
moejoe95 <j.gugglberger@gmail.com>
2020-04-02 09:42:34 +0200

    project world coord. into both image frames

--- src/stereo_manual.py
+++ src/stereo_manual.py
@@ -5,47 +5,34 @@ from Constants import *
 import pandas as pd
 
 
-def draw_lines(im, center, obj_pts):
-    for pt in obj_pts:
-        cv2.line(im, center, pt, (255, 0, 0), 5)
+def draw(img, imgpts):
+    imgpts = np.int32(imgpts).reshape(-1, 2)
+    center = (464, 302)
+    cv2.line(img, center, tuple(imgpts[0].ravel()), (255, 0, 0), 5)
+    cv2.line(img, center, tuple(imgpts[1].ravel()), (0, 255, 0), 5)
+    cv2.line(img, center, tuple(imgpts[2].ravel()), (0, 0, 255), 5)
 
 def draw_points(img, pts):
     for pt in pts:
         cv2.circle(img, (pt[0],pt[1]), 3, (0, 0, 255), -1)
 
-# transforms point according to formula p2 = R*p1 + t returns a tuple (x, y)
-def transform_point(pt1, R, t):
-    # from image to homogeneuos
-    pt1_homo = np.append(pt1, 1).reshape(3,1)
-    # map from img1 to img2
-    pt_rot = np.matmul(R, pt1_homo)
-    pt2_hom = np.add(pt_rot, t) 
-    # back from homogeneous to image
-    pt2 = pt2_hom / pt2_hom[2]
-    # reshape to int tuple
-    return (int(pt2[0][0]), int(pt2[1][0])) 
-
-
 # read manuel points
 pts1 = pd.read_csv('../tmp/manual_pt1.csv', sep=',').values
 pts2 = pd.read_csv('../tmp/manual_pt2.csv', sep=',').values
 
+# Load previously saved data
+K, dist = np.load(MAT_CAMERA), np.load(MAT_DIST_COEFF)
+
 img1 = cv2.imread('../data/img1.jpg')
 
-center = (464, 302)
-obj_pts1 = [(464,252), (514,302),(428,338)]
 
-img1 = cv2.resize(img1, (960, 540))
-draw_lines(img1, center, obj_pts1)
-draw_points(img1, pts1)
+obj = np.float32([[1,0,0], [0,1,0], [0,0,-1]]).reshape(-1,3)
 
-cv2.imshow('img1', img1)
-cv2.waitKey(0)
+# project world coordinates to frame 1
+r_vec_id, _ = cv2.Rodrigues(np.identity(3))
+imgpts1, _ = cv2.projectPoints(obj, r_vec_id, np.zeros(3), K, dist)
 
 # map to second image
-
-# Load K and dist from calibration
-K, dist = np.load(MAT_CAMERA), np.load(MAT_DIST_COEFF)
 
 # https://stackoverflow.com/questions/33906111/how-do-i-estimate-positions-of-two-cameras-in-opencv
 # Normalize for Essential Matrix calaculation
@@ -55,7 +42,7 @@ pts_r_norm = cv2.undistortPoints(np.expand_dims(pts2, axis=1).astype(dtype=np.fl
 E, _ = cv2.findEssentialMat(pts_l_norm, pts_r_norm, method=cv2.RANSAC, prob=0.999, threshold=0.1) # TODO test different settings
 
 # recover relative camera rotation and translation from essential matrix and the corresponding points
-_, R, t, _ = cv2.recoverPose(E, pts1, pts2)
+points, R, t, _ = cv2.recoverPose(E, pts1, pts2)
 
 # IMPORTANT: R and t may need to be inverted:
 # see https://answers.opencv.org/question/31421/opencv-3-essentialmatrix-and-recoverpose/
@@ -63,22 +50,23 @@ _, R, t, _ = cv2.recoverPose(E, pts1, pts2)
 #R = np.linalg.inv(R)
 #t = np.flip(t)
 
-# transform points
-obj_pts2 = []
-for imagept in obj_pts1:
-    obj_pts2.append(transform_point(imagept, R, t))
-    
-# transform center
-center = transform_point(center, R, t)
-
-print(obj_pts2)
-print(center)
-
 img2 = cv2.imread('../data/img2.jpg')
-img2 = cv2.resize(img2, (960, 540))
-draw_lines(img2, center, obj_pts2)
-draw_points(img2, pts2)
 
+# project world coordinates to frame 2
+r_vec, _ = cv2.Rodrigues(R)
+imgpts2, _ = cv2.projectPoints(obj, r_vec, t, K, dist)
+
+
+# show images
+
+img1 = cv2.resize(img1, (960, 540))
+draw_points(img1, pts1)
+draw(img1, imgpts1)
+cv2.imshow('img1', img1)
+
+img2 = cv2.resize(img2, (960, 540))
+draw_points(img2, pts2)
+draw(img2, imgpts1)
 cv2.imshow('img2', img2)
 cv2.waitKey(0)
 cv2.destroyAllWindows()

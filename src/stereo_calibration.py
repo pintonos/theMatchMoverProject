f82c15eea44f27b088943d74e204bf79cc257123
Andreas Peintner <a.peintner@gmx.net>
2020-04-03 17:08:34 +0200

    update stereo calibration

--- src/stereo_calibration.py
+++ src/stereo_calibration.py
@@ -1,24 +1,54 @@
 import cv2
 import numpy as np
-from matplotlib import pyplot as plt
 from Constants import *
 
-MAX_FPS = 100
+SKIP_FPS = 150
+MAX_FPS = 195
+SCALING_FACTOR = 9
 
-def draw(img, corners, imgpts):
+def draw(img, imgpts):
     imgpts = np.int32(imgpts).reshape(-1, 2)
-    img = cv2.line(img, tuple(corners[0]), tuple(imgpts[0].ravel()), (255,0,0), 5)
-    img = cv2.line(img, tuple(corners[1]), tuple(imgpts[1].ravel()), (0,255,0), 5)
-    img = cv2.line(img, tuple(corners[2]), tuple(imgpts[2].ravel()), (0,0,255), 5)
+
+    # draw ground floor in green
+    img = cv2.drawContours(img, [imgpts[:4]], -1, (0, 255, 0), -3)
+
+    # draw pillars in blue color
+    for i, j in zip(range(4), range(4, 8)):
+        img = cv2.line(img, tuple(imgpts[i]), tuple(imgpts[j]), (255), 3)
+
+    # draw top layer in red color
+    img = cv2.drawContours(img, [imgpts[4:]], -1, (0, 0, 255), 3)
+
     return img
 
 
-axis = np.float32([[3,0,0], [0,3,0], [0,0,-3]]).reshape(-1,3)
-corners = np.float32([[858, 580], [858, 580], [858, 580]])
+def get_harris_corner(img):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray = np.float32(gray)
+    dst = cv2.cornerHarris(gray, 2, 3, 0.04)
+    dst = cv2.dilate(dst, None)
+    ret, dst = cv2.threshold(dst, 0.01 * dst.max(), 255, 0)
+    dst = np.uint8(dst)
+    return dst
+
+
+# Load previously saved data
+K, dist = np.load(MAT_CAMERA), np.load(MAT_DIST_COEFF)
+
+obj = np.float32([[0, 0, 0], [0, 1, 0], [1, 1, 0], [1, 0, 0], [0, 0, -1], [0, 1, -1], [1, 1, -1], [1, 0, -1]])
 
 video_cap = cv2.VideoCapture(VIDEO_PATH)
 number_frames = int(video_cap.get(cv2.CAP_PROP_FRAME_COUNT))
 print('read', number_frames, 'frames ...')
+
+# project world coordinates to frame 1
+r_vec_id, _ = cv2.Rodrigues(np.identity(3))
+t_vec = np.float32(np.asarray([0, 0, SCALING_FACTOR]))
+imgpts1, _ = cv2.projectPoints(obj, r_vec_id, t_vec, K, dist)
+
+success, img1 = video_cap.read()
+dst1 = get_harris_corner(img1)
+gray1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
 
 # Get the Default resolutions
 frame_width = int(video_cap.get(3))
@@ -28,25 +58,28 @@ frame_height = int(video_cap.get(4))
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
 out = cv2.VideoWriter(VIDEO_OUT_STEREO, fourcc, 20.0, (frame_width, frame_height))
 
+out.write(img1)
+
 frame_counter = 0
 success = True
-img1, img2 = 0, 0
+img2 = 0
 while success and frame_counter < MAX_FPS:
-    if frame_counter == 0:
-        success, img1 = video_cap.read()
-        frame_counter = frame_counter + 1
-        continue
-
     frame_counter = frame_counter + 1
+    success, img2 = video_cap.read()
     print("Frame: {}/{}".format(frame_counter, number_frames))
 
-    success, img2 = video_cap.read()
+    if frame_counter < SKIP_FPS:
+        continue
 
     sift = cv2.xfeatures2d.SIFT_create()
 
+    gray2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+
+    #dst2 = get_harris_corner(img2)
+
     # find the keypoints and descriptors with SIFT
-    kp1, des1 = sift.detectAndCompute(img1, None)
-    kp2, des2 = sift.detectAndCompute(img2, None)
+    kp1, des1 = sift.detectAndCompute(gray1, None)
+    kp2, des2 = sift.detectAndCompute(gray2, None)
 
     # FLANN parameters
     FLANN_INDEX_KDTREE = 0
@@ -70,43 +103,22 @@ while success and frame_counter < MAX_FPS:
     pts1 = np.int32(pts1)
     pts2 = np.int32(pts2)
 
-    # get Fundamental matrix
-    F, mask = cv2.findFundamentalMat(pts1, pts2, cv2.FM_LMEDS)
-    print("F:")
-    print(F)
-
-    # We select only inlier points
-    pts1 = pts1[mask.ravel() == 1]
-    pts2 = pts2[mask.ravel() == 1]
-
-    # Load previously saved data
-    K, dist = np.load(MAT_CAMERA), np.load(MAT_DIST_COEFF)
-
-    # get E from equation: E = K'^T * F * K
-    E = np.matmul(np.matmul(np.transpose(K), F), K)
-
-    print("E:")
-    print(E)
+    E, _ = cv2.findEssentialMat(pts1, pts2, method=cv2.RANSAC, prob=0.999, threshold=0.01,
+                                cameraMatrix=K)
 
     # recover relative camera rotation and translation from essential matrix and the corresponding points
-    inlier_points, R, t, _ = cv2.recoverPose(E, pts1, pts2, K)
+    points, R, t, _ = cv2.recoverPose(E, pts1, pts2, K)
 
-    print(inlier_points, 'inliers points found')
-    print("R:")
-    print(R)
-    print("t:")
-    print(t, '\n')
+    # project world coordinates to frame 2
+    t += np.expand_dims(t_vec, axis=1)  # add scaling factor
+    r_vec, _ = cv2.Rodrigues(R, dst=dist)
+    imgpts2, _ = cv2.projectPoints(obj, r_vec, t, K, dist)
 
-    # project 3D points to image plane
-    imgpts, jac = cv2.projectPoints(axis, R, t, K, dist)
+    img2 = draw(img2, imgpts2)
+    cv2.imshow('img', img2)
+    cv2.waitKey(50)
 
-    img = draw(img2, corners, imgpts)
-    #cv2.imshow('img', img)
-    #cv2.waitKey(50)
-
-    out.write(img)
-
-    img1 = img2
+    out.write(img2)
 
 
 video_cap.release()

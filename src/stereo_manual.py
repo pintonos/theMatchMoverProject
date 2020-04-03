7ad80a4a696bf7caa760fc7ed014f095f80f64b7
Tobias Kupek <mail@tkupek.eu>
2020-04-03 12:14:40 +0200

    another solution attempt to play with

--- src/stereo_manual.py
+++ src/stereo_manual.py
@@ -4,6 +4,8 @@ from matplotlib import pyplot as plt
 from src.Constants import *
 import pandas as pd
 
+SCALING_FACTOR = 9
+
 
 def draw(img, imgpts):
     imgpts = np.int32(imgpts).reshape(-1, 2)
@@ -53,59 +55,60 @@ def invert(R, t):
     return R_inv, t_inv
 
 
-
-# read manuel points
+# read manual points
 pts1 = pd.read_csv('../tmp/manual_pt1.csv', sep=',').values
 pts2 = pd.read_csv('../tmp/manual_pt2.csv', sep=',').values
+
+# adjust points to real scaling
+pts1 *= 2
+pts2 *= 2
 
 # Load previously saved data
 K, dist = np.load(MAT_CAMERA), np.load(MAT_DIST_COEFF)
 
-obj = np.float32([[0,0,0], [1,0,0], [0,1,0], [0,0,1], [1,1,0], [1,0,1], [0,1,1], [1,1,1]])
+obj = np.float32([[0,0,0], [1,0,0], [0,1,0], [0,0,-1], [1,1,0], [1,0,-1], [0,1,-1], [1,1,-1]])
 
 # project world coordinates to frame 1
-
-t_vec_init = np.float32(np.asarray([1,1,-10])).reshape(3,1)
-r_vec_init, _ = cv2.Rodrigues(np.identity(3))
-
-imgpts1, _ = cv2.projectPoints(obj, r_vec_init, t_vec_init, K, dist)
+r_vec_id, _ = cv2.Rodrigues(np.identity(3))
+t_vec = np.float32(np.asarray([0, 0, SCALING_FACTOR]))
+imgpts1, _ = cv2.projectPoints(obj, r_vec_id, t_vec, K, dist)
 
 # map to second image
 
 # https://stackoverflow.com/questions/33906111/how-do-i-estimate-positions-of-two-cameras-in-opencv
-# Normalize for Essential Matrix calaculation
-pts_l_norm = cv2.undistortPoints(np.expand_dims(pts1, axis=1).astype(dtype=np.float32), cameraMatrix=K, distCoeffs=dist)
-pts_r_norm = cv2.undistortPoints(np.expand_dims(pts2, axis=1).astype(dtype=np.float32), cameraMatrix=K, distCoeffs=dist)
+# Normalize for Essential Matrix calculation
+# pts_l_norm = cv2.undistortPoints(np.expand_dims(pts1, axis=1).astype(dtype=np.float32), cameraMatrix=K, distCoeffs=dist)
+# pts_r_norm = cv2.undistortPoints(np.expand_dims(pts2, axis=1).astype(dtype=np.float32), cameraMatrix=K, distCoeffs=dist)
 
-E, _ = cv2.findEssentialMat(pts_l_norm, pts_r_norm, method=cv2.RANSAC, prob=0.999, threshold=0.1, cameraMatrix=K) # TODO test different settings
+E, _ = cv2.findEssentialMat(pts1, pts2, method=cv2.RANSAC, prob=0.999, threshold=0.1, cameraMatrix=K) # TODO test different settings
 
 # recover relative camera rotation and translation from essential matrix and the corresponding points
-points, R, t, _ = cv2.recoverPose(E, pts_l_norm, pts_r_norm)
-print(points)
+points, R, t, _ = cv2.recoverPose(E, pts1, pts2, K)
+
 # IMPORTANT: R and t may need to be inverted:
 # see https://answers.opencv.org/question/31421/opencv-3-essentialmatrix-and-recoverpose/
 # and https://stackoverflow.com/questions/54486906/correctly-interpreting-the-pose-rotation-and-translation-after-recoverpose-f
-
-# invert R and t
-#R, t = invert(R, t)
-r_vec, _ = cv2.Rodrigues(R, dst=dist)
-t = np.add(t, t_vec_init)
+# R = np.linalg.inv(R)
+# t = np.flip(t)
 
 # project world coordinates to frame 2
+t += np.expand_dims(t_vec, axis=1) # add scaling factor
+r_vec, _ = cv2.Rodrigues(R, dst=dist)
 imgpts2, _ = cv2.projectPoints(obj, r_vec, t, K, dist)
 
 
 # show images
 img1 = cv2.imread('../data/img1.jpg')
-img1 = cv2.resize(img1, (960, 540))
+img2 = cv2.imread('../data/img2.jpg')
+
 draw_points(img1, pts1)
 draw(img1, imgpts1)
+img1 = cv2.resize(img1, (960, 540))
 cv2.imshow('img1', img1)
 
-img2 = cv2.imread('../data/img2.jpg')
-img2 = cv2.resize(img2, (960, 540))
 draw_points(img2, pts2)
 draw(img2, imgpts2)
+img2 = cv2.resize(img2, (960, 540))
 cv2.imshow('img2', img2)
 cv2.waitKey(0)
 cv2.destroyAllWindows()

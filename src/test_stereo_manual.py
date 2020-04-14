4279da8e1266f599cc03ade4153d0a253c3e4be5
moejoe95 <j.gugglberger@gmail.com>
2020-04-14 17:58:20 +0200

    update stereo manual

--- src/test_stereo_manual.py
+++ src/test_stereo_manual.py
@@ -14,9 +14,9 @@ if K is None or dist is None:
 
 # Read manual points
 # Scale *2, points were matched in resized img
-match_points_1 = pd.read_csv('../' + MANUAL_MATCH_POINTS_1, sep=',').values * 2
-match_points_2 = pd.read_csv('../' + MANUAL_MATCH_POINTS_2, sep=',').values * 2
-match_points_3 = pd.read_csv('../' + MANUAL_MATCH_POINTS_3, sep=',').values * 2
+match_points_1 = pd.read_csv('./' + MANUAL_MATCH_POINTS_1, sep=',').values * 2
+match_points_2 = pd.read_csv('./' + MANUAL_MATCH_POINTS_2, sep=',').values * 2
+match_points_3 = pd.read_csv('./' + MANUAL_MATCH_POINTS_3, sep=',').values * 2
 
 # Points for a 3D cube
 img_points_3d = get_3d_cube_points()
@@ -27,13 +27,13 @@ t_vec = OBJECT_POSITION
 proj_points_img_1, _ = cv2.projectPoints(img_points_3d, r_vec_id, t_vec, K, dist)
 
 # Map points to second and third image
-proj_points_img_2 = stereo_view_map(match_points_1, match_points_2, img_points_3d, t_vec, OBJECT_ORIENTATION, K, dist)
-proj_points_img_3 = stereo_view_map(match_points_1, match_points_3, img_points_3d, t_vec, OBJECT_ORIENTATION, K, dist)
+_, _, proj_points_img_2 = stereo_view_map(match_points_1, match_points_2, img_points_3d, t_vec, OBJECT_ORIENTATION, K, dist)
+_, _, proj_points_img_3 = stereo_view_map(match_points_1, match_points_3, img_points_3d, t_vec, OBJECT_ORIENTATION, K, dist)
 
 # Read and plot images
-img_1 = cv2.imread('../' + DATA_PATH + 'img_1.jpg')
-img_2 = cv2.imread('../' + DATA_PATH + 'img_2.jpg')
-img_3 = cv2.imread('../' + DATA_PATH + 'img_3.jpg')
+img_1 = cv2.imread('./' + DATA_PATH + 'img_1.jpg')
+img_2 = cv2.imread('./' + DATA_PATH + 'img_2.jpg')
+img_3 = cv2.imread('./' + DATA_PATH + 'img_3.jpg')
 
 draw_points(img_1, match_points_1)
 draw_points(img_2, match_points_2)

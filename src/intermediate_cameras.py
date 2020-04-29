ecccdefa1fc2c5e8565c3bc8fdb8b1ffa001d80f
Tobias Kupek <mail@tkupek.eu>
2020-04-29 12:17:44 +0200

    add find_trace_points method

--- src/intermediate_cameras.py
+++ src/intermediate_cameras.py
@@ -38,7 +38,58 @@ while success and count < 100:
     break
 
 
-def find_trace_points(video, id1, id2):
+def find_trace_points(video, idx1, idx2):
+    '''
+    finds point matches that are preserved between idx1 and idx2
+    :param video: video file with more than |idx2| frames
+    :param idx1: index to start with
+    :param idx2: index to end with
+    :return: list of dict with structure [{'startPoint': 2DPoint, 'endPoint': 2DPoint}],
+        startPoint is the 2DPoint of the first frame, endPoint is the 2DPoint of the last frame
+    '''
+    if idx2 - idx1 <= 0:
+        print("warning, called find_trace_points with 0 or negative frame indexes")
+        return []
+
+    curr_idx = -1
+    success = True
+    traced_matches = None
+    last_frame = None
+
+    while success:
+        success, frame = video.read()
+        curr_idx += 1
+
+        if curr_idx == idx1:
+            # start tracing
+            last_frame = frame
+
+        if curr_idx > idx1:
+            # trace
+            match_points_1, match_points_2, matches = get_points(last_frame, frame)
+
+            if traced_matches is None:
+                traced_matches = [{
+                    'startPoint': match_points_1[i],
+                    'from': x.queryIdx,
+                    'to': x.trainIdx,
+                    'endPoint': match_points_2[i]} for i, x in enumerate(matches)]
+            else:
+                new_matches = dict([(x.queryIdx, x.trainIdx) for x in matches])
+                for match in traced_matches:
+                    new_from = match['to']
+                    if new_from in new_matches:
+                        match['from'] = new_from
+                        match['to'] = new_matches[new_from]
+                        match['endPoint'] = match_points_2[list(new_matches.keys()).index(new_from)]
+                    else:
+                        match['to'] = None
+
+                traced_matches = list(filter(lambda m: m['to'] is None, traced_matches))
+
+        if curr_idx > idx2:
+            # end tracing
+            break
 
     return traced_matches
 

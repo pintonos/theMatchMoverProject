2b5770d230bd12c52aeb61975766fcdbf96679d0
Andreas Peintner <a.peintner@gmx.net>
2020-05-06 14:48:50 +0200

    refactor key frame finding

--- src/triangulate_resection.py
+++ src/triangulate_resection.py
@@ -10,11 +10,11 @@ import pandas as pd
 if K is None or dist is None:
     raise Exception('Camera matrix or distortion coefficient not found')
 
-MAX_FPS = 50
+MAX_FPS = 40
 MIN_MATCHES = 200
 
 
-def find_key_frames(video, idx1, idx2, early_stoppage=False):
+def find_next_key_frame(video, idx1, idx2):
     '''
     finds point matches that are preserved between idx1 and idx2
     :param video: video file with more than |idx2| frames
@@ -32,17 +32,19 @@ def find_key_frames(video, idx1, idx2, early_stoppage=False):
     success = True
     keyframes = []
     traced_matches = None
-    last_frame = None
-    while success and curr_idx < idx2 - 1:
+    prev_frame = None
+    new_keyframe_pos = 0
+    keyframe_found = False
+    while success and curr_idx < idx2 - idx1 - 1:
         success, frame = video.read()
         curr_idx += 1
 
         if curr_idx <= idx1:
-            last_frame = frame
+            prev_frame = frame
             continue
 
         # trace
-        match_points_1, match_points_2, matches = get_points(last_frame, frame)  # TODO check if order is correct
+        match_points_1, match_points_2, matches = get_points(prev_frame, frame)
 
         if traced_matches is None:
             traced_matches = [{
@@ -65,18 +67,18 @@ def find_key_frames(video, idx1, idx2, early_stoppage=False):
 
             if len(traced_matches) <= MIN_MATCHES:
                 # new keyframe
-                print('found keyframe at pos ' + str(curr_idx + idx1))
+                new_keyframe_pos = curr_idx + idx1 + 1
+                print('found keyframe at pos ' + str(new_keyframe_pos))
                 keyframes.append(traced_matches)
-                traced_matches = None
-                if early_stoppage:
-                    break
+                keyframe_found = True
+                break
 
-        last_frame = frame
-
-    if not early_stoppage:
-        keyframes.append(traced_matches)
-
-    return keyframes
+        prev_frame = frame
+    if keyframe_found:
+        return keyframes, new_keyframe_pos
+    else:
+        print('no new keyframe found')
+        return keyframes, None
 
 
 # get point correspondences
@@ -88,16 +90,15 @@ pts100 = pd.read_csv(REF_POINTS_100, sep=',', header=None, dtype=float).values
 
 reader, writer = get_video_streams()
 
-keyframes = find_key_frames(reader, 0, MAX_FPS, early_stoppage=True)
-curr_idx = 0
-while curr_idx < MAX_FPS:
-    curr_idx = curr_idx + len(keyframes[-1][0]['coordinates'])
+keyframes, keyframe_idx = find_next_key_frame(reader, 0, MAX_FPS)
+while keyframe_idx and keyframe_idx < MAX_FPS:
     if len(keyframes) > 1:
-        range_idx = curr_idx - len(keyframes[-2][0]['coordinates'])
+        range_idx = keyframe_idx - len(keyframes[-2][0]['coordinates'])
     else:
         range_idx = len(keyframes[-1][0]['coordinates'])
 
-    keyframes = keyframes + find_key_frames(reader, curr_idx - (range_idx // 2), MAX_FPS, early_stoppage=True)
+    tmp_kf, keyframe_idx = find_next_key_frame(reader, keyframe_idx - (range_idx // 2), MAX_FPS)
+    keyframes = keyframes + tmp_kf
 
 keyframe_pts = []
 for k in keyframes:
@@ -108,49 +109,53 @@ for k in keyframes:
             pts.append(frame['coordinates'][i])
         pts_list.append(pts)
     keyframe_pts.append(np.asarray(pts_list))
+'''
+# check match points
+img = cv2.imread(DATA_PATH + 'img_10.jpg')
+draw_points(img, keyframe_pts[1][0])
+cv2.imshow('img_10', cv2.resize(img, DEMO_RESIZE))
 
-#print(keyframe_pts)
+img = cv2.imread(DATA_PATH + 'img_38.jpg')
+draw_points(img, keyframe_pts[1][-1])
+cv2.imshow('img_38', cv2.resize(img, DEMO_RESIZE))
 
-R1, t1 = INIT_ORIENTATION, INIT_POSITION
-R2, t2 = get_R_and_t(keyframe_pts[0][0], keyframe_pts[0][18], K)
+cv2.waitKey(0)'''
+
+R0, t0 = INIT_ORIENTATION, INIT_POSITION
+R2, t2 = get_R_and_t(keyframe_pts[0][0], keyframe_pts[0][-1], K)
 
 # get world points of axis
-axis, _ = get_3d_world_points(R1, t1, R2, t2, pts0, pts18, dist, K)
+axis, _ = get_3d_world_points(R0, t0, R2, t2, pts0, pts18, dist, K)
 # print(axis)
 # axis = get_3d_axis()
 # print(axis)
 
 # get world points
-_, world_coords = get_3d_world_points(R1, t1, R2, t2, keyframe_pts[0][0], keyframe_pts[0][18], dist, K)
+_, world_coords1 = get_3d_world_points(R0, t0, R2, t2, keyframe_pts[0][0], keyframe_pts[0][-1], dist, K)
+_, R1, t1, _ = cv2.solvePnPRansac(world_coords1, keyframe_pts[0][9], K, dist,
+                                  reprojectionError=20.0)
 
-ret, R, T, inliers = cv2.solvePnPRansac(world_coords, keyframe_pts[0][12], K, dist,
+points2d, _ = cv2.projectPoints(axis, R1, t1, K, dist)
+
+# works
+#img = cv2.imread(DATA_PATH + 'img_9.jpg')
+#draw_points(img, functools.reduce(operator.iconcat, points2d.astype(int).tolist(), []))
+#cv2.imshow('img_9', cv2.resize(img, DEMO_RESIZE))
+
+
+# check R2, t2 with new approach
+R1, _ = cv2.Rodrigues(R1, dst=dist)
+R3, t3 = get_R_and_t(keyframe_pts[1][0], keyframe_pts[1][-1], K)
+_, world_coords2 = get_3d_world_points(R1, t1, R3, t3, keyframe_pts[1][0], keyframe_pts[1][-1], dist, K)
+
+_, R2, t2, _ = cv2.solvePnPRansac(world_coords2, keyframe_pts[1][7], K, dist,
                                         reprojectionError=20.0)
 
-points2d, _ = cv2.projectPoints(axis, R, T, K, dist)
+points2d, _ = cv2.projectPoints(get_3d_axis(), R2, t2, K, dist) # TODO check 3d axis?
 
-img = cv2.imread(DATA_PATH + 'img_12.jpg')
+img = cv2.imread(DATA_PATH + 'img_18.jpg')
 draw_points(img, functools.reduce(operator.iconcat, points2d.astype(int).tolist(), []))
-cv2.imshow('img', cv2.resize(img, DEMO_RESIZE))
+cv2.imshow('img_18', cv2.resize(img, DEMO_RESIZE))
 cv2.waitKey(0)
-
-'''
-img = cv2.imread(DATA_PATH + 'img_0.jpg')
-points2d, _ = cv2.projectPoints(axis, R1, t1, K, dist)
-draw_points(img, functools.reduce(operator.iconcat, points2d.astype(int).tolist(), []))
-cv2.imshow('img0', img)
-
-img = cv2.imread(DATA_PATH + 'img_34.jpg')
-points2d, _ = cv2.projectPoints(axis, R2, t2, K, dist)
-draw_points(img, functools.reduce(operator.iconcat, points2d.astype(int).tolist(), []))
-cv2.imshow('img34', img)
-
-_, world_coords = get_3d_world_points(R1, t1, R2, t2, keyframe_pts[0], keyframe_pts[34], dist, K)
-
-ret, mtx, dist_tmp, rvecs, tvecs = cv2.calibrateCamera([world_coords],
-                                                       [keyframe_pts[0]],
-                                                       (1080, 1920),
-                                                       cameraMatrix=K, distCoeffs=dist,
-                                                       flags=cv2.CALIB_USE_INTRINSIC_GUESS)
-'''
 
 

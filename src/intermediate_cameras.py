f98ba2b4ea2f1b257efec3bed2b0f039893fcbc9
Andreas Peintner <a.peintner@gmx.net>
2020-05-11 11:04:27 +0200

    fix intermediate cameras handling

--- src/intermediate_cameras.py
+++ src/intermediate_cameras.py
@@ -5,7 +5,7 @@ from functions import *
 import cv2
 
 reader, writer = get_video_streams()
-MAX_FPS = 250  # int(reader.get(cv2.CAP_PROP_FRAME_COUNT))
+MAX_FPS = 200  # int(reader.get(cv2.CAP_PROP_FRAME_COUNT))
 
 keyframes, keyframe_idx = find_next_key_frame(0, MAX_FPS)
 while keyframe_idx and keyframe_idx < MAX_FPS:
@@ -34,28 +34,31 @@ axis = get_3d_axis(R2, t2)
 _, world_coords = get_3d_world_points(R0, t0, R2, t2, keyframe_pts[0][0], keyframe_pts[0][-1], dist, K)
 
 for i, keyframe in enumerate(keyframe_pts):
-
+    print(i)
     # new keyframe by resectioning
     if i != 0:
         R_half, _ = cv2.Rodrigues(R_half)
         R, _ = cv2.Rodrigues(R)
-        _, world_coords = get_3d_world_points(R_half, t_half, R, t, keyframe_pts[i][0], keyframe_pts[i][-1], dist, K)
+        _, world_coords = get_3d_world_points(R_half, t_half, R, t, keyframe_pts[i][0], keyframe_pts[i][len(keyframe_pts[i])//2 - 1], dist, K)
         _, R, t, _ = cv2.solvePnPRansac(world_coords, keyframe_pts[i][-1], K, dist, reprojectionError=1.0)
 
     # fill between keyframes
     for j, frame in enumerate(keyframe_pts[i]):
         _, R, t, _ = cv2.solvePnPRansac(world_coords, keyframe_pts[i][j], K, dist, reprojectionError=1.0) # TODO check reprojectionError makes big difference!
+
         if j == len(keyframe_pts[i]) // 2:
             R_half, t_half = R, t
-        points2d, _ = cv2.projectPoints(axis, R, t, K, dist)
 
-        _, img = reader.read()
-        draw_axis(img, points2d)
+        if j < len(keyframe_pts[i]) // 2:
+            points2d, _ = cv2.projectPoints(axis, R, t, K, dist)
 
-        writer.write(img)
+            _, img = reader.read()
+            draw_axis(img, points2d)
 
-        cv2.imshow('img', cv2.resize(img, DEMO_RESIZE))
-        cv2.waitKey(500)
+            writer.write(img)
+
+            cv2.imshow('img', cv2.resize(img, DEMO_RESIZE))
+            cv2.waitKey(500)
 
 
 reader.release()

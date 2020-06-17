0bc90f3afb68969c8fdd7e16c338abe5ea8d8906
moejoe95 <j.gugglberger@gmail.com>
2020-06-17 12:15:37 +0200

    update configuration management

--- src/util/config.py
+++ src/util/config.py
@@ -6,42 +6,45 @@ Paths for INPUT and OUTPUT video files
 Paths for INTERMEDIATE files
 """
 
-# Data & Tmp path
+# Temporary file paths
 DATA_PATH = '../resources/data/'
-
-# Path to calibration video
-CALIB_VIDEO_PATH = DATA_PATH + 'visual-geometry-calibration.MTS'
-
-# Path to input video
-VIDEO_PATH = DATA_PATH + 'visual-geometry-video3.MTS'
-
-# Path to output video
-VIDEO_OUT_PATH = DATA_PATH + 'output.avi'
-
-# saved keyframe tracing files
 KEYFRAMES_PATH = DATA_PATH + 'keyframes.npy'
 KEYFRAMES_IDX_PATH = DATA_PATH + 'keyframe_idx.npy'
-
-# Calibration files
 CAMERA_MATRIX = DATA_PATH + 'cmatrix.npy'
 CAMERA_DIST_COEFF = DATA_PATH + 'dist.npy'
+CONFIG_PATH = 'config.txt'
 
 # template CSV file path
 REF_POINTS = DATA_PATH + 'reference_{frame}.csv'
 
+# define global variables
+global K
+global dist
+global CALIB_VIDEO_PATH
+global VIDEO_PATH
+global VIDEO_OUT_PATH
 
-def load_interm(camera_matrix, camera_dist_coeff):
-    global K
-    global dist
+try:
+    K = np.load(CAMERA_MATRIX)
+    dist = np.load(CAMERA_DIST_COEFF)
+except IOError:
+    print('ERROR: No camera matrix or distortion coefficient initialized. Calibrate camera first.')
+    exit(-1)
 
-    try:
-        K = np.load(camera_matrix)
-        dist = np.load(camera_dist_coeff)
-    except IOError:
-        print('ERROR: No camera matrix or distortion coefficient initialized. Calibrate camera first.')
-        exit(-1)
+try:
+    with open(CONFIG_PATH) as config_file:
+        lines = config_file.read().splitlines()
+        args = {}
+        for line in lines:
+            name, var = line.partition("=")[::2]
+            args[name.strip()] = var
+
+        CALIB_VIDEO_PATH = args['calib_video_path']
+        VIDEO_PATH = args['input_video_path']
+        VIDEO_OUT_PATH = args['output_video_path']
+except IOError:
+    print('ERROR: Config.txt is missing or invalid.')
+    exit(-1)
 
 
-# init K and dist
-K, dist = None, None
-load_interm(CAMERA_MATRIX, CAMERA_DIST_COEFF)
+
